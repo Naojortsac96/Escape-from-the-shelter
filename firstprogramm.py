c8613afcccd6d71f9f233628116439e0450f9625
Joan Garcia <92384786+Naojortsac96@users.noreply.github.com>
2026-04-08 13:01:15 +0100

    Delete firstprogramm.py

--- firstprogramm.py
+++
@@ -1,9 +0,0 @@
-a=int(input("Introduzca su primer número: "))
-b=int(input("Introduzca su segundo numero: "))
-c= int(input("Introduzca su tercer numero: "))
-
-d=(a,b,c)
-
-print("El número más grande entre {},{},{} es:{}, y el más pequeno es:{}".format(a,b,c,
-                                                                                 max(a,b,c),
-                                                                                 min(a,b,c)))
